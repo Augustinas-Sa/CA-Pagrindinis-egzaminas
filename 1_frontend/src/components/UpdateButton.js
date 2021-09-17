b39f3e70cdd09df90eb2dcb40add7a5bb15aaa82
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-17 15:55:23 +0300

    created update button starter

---
+++ 1_frontend/src/components/UpdateButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './UpdateButton.css';
+
+const UpdateButton = () => {
+  return <button className='update-btn'>Atnaujinti vartotoją</button>;
+};
+
+export default UpdateButton;
