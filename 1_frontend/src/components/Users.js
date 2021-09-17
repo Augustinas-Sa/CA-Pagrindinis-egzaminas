2be78c454013259caf8daea88e80683017f6293b
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-09-17 11:07:54 +0300

    created get users logic

---
+++ 1_frontend/src/components/Users.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Users.css';
+
+const GET_ALL_USERS = 'http://localhost:5000/api/users';
+
+const Users = () => {
+  // Hooks
+  // - local state
+  const [users, setUsers] = useState([]);
+
+  // - side effects
+  useEffect(() => {
+    axios.get(GET_ALL_USERS).then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  return (
+    <main>
+      <section>
+        <h2 className='section-name'>Visi vartotojai</h2>
+        <div>
+          {users.map((item) => (
+            <div className='card_shadow' key={item._id}>
+              <p>Vardas: {item.name}</p>
+              <p>Amžius: {item.age}</p>
+              <p>El. paštas: {item.email}</p>
+              <p>Slaptažodis: {item.password}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Users;
